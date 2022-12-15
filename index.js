45cfb2a0e1f0c91f047b787d46f801d83740c1a8
teedaa <pengtedachengllhs@gmail.com>
2022-12-14 21:21:28 -0800

    Employee constructor made

--- index.js
+++ index.js
@@ -188,3 +188,27 @@ const addEmployee = () => {
     })
 
 };
+const writeFile = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+}; 
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
